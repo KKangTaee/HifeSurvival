68c678e132c3a18a40a9f4305ee105f228624720
kangTae <honeypipeline@gmail.com>
2023-06-12 06:56:27 +0900

    [클라/수정/기타] 불필요한 코드 삭제 및 월드아이템 클래스 추가

---
+++ HifeSurvival/Assets/Scripts/WorldMap/WorldItem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldItem : WorldObjectBase
+{
+    public void DropItem()
+    {
+
+    }
+
+    public void GetItem()
+    {
+        
+    }
+}

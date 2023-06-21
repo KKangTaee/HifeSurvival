c431276e9b54231e183847a9f4f341edbfe73202
Yodle_94 <sj_artist@naver.com>
2023-06-21 10:57:19 +0900

    [공용모듈/구현/타이머] 서버 클라 공용으로 사용할 타이머 클래스 추가.

---
+++ HifeSurvival/RealtimeServer/ServerCore/HTimer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerCore
+{
+    class HTimer
+    {
+        public static long GetCurrentTimestamp()
+        {
+            var dto = new DateTimeOffset();
+            return dto.ToUnixTimeMilliseconds();
+        }
+    }
+}

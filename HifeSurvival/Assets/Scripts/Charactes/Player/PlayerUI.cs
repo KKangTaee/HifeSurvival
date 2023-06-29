5d1eb4a58b9460000b0859b8027a38131cb8ecf7
KKangTaee <honeypipeline@gmail.com>
2023-06-29 22:40:29 +0900

    [클라/수정/플레이어] 플레이어 UI 수정
    * HP 감소 안되는 이슈 수정

--- HifeSurvival/Assets/Scripts/Charactes/Player/PlayerUI.cs
+++ HifeSurvival/Assets/Scripts/Charactes/Player/PlayerUI.cs
@@ -6,8 +6,11 @@ using DG.Tweening;
 
 public class PlayerUI : MonoBehaviour
 {
-    [SerializeField] Image IMG_hpInner;
-    [SerializeField] Image IMG_hpBar;
+    [SerializeField] Slider SLD_hpInner;
+    [SerializeField] Slider SLD_hpBar;
+    [SerializeField] Image  IMG_hpInnerFill;
+    [SerializeField] Image  IMG_hpBarFill;
+
 
     private int _maxHP;
     private int _currHP;
@@ -50,24 +53,24 @@ public class PlayerUI : MonoBehaviour
     {
         _currHP -= damageValue;
 
-        IMG_hpBar.fillAmount    = (float)_currHP / _maxHP;
+        SLD_hpBar.value    = (float)_currHP / _maxHP;
 
         // 번쩍이는 효과를 위해 색상을 잠시 흰색으로 변경
-        Color originalColor = IMG_hpInner.color;
-        IMG_hpInner.color = Color.white;
+        Color originalColor = IMG_hpInnerFill.color;
+        IMG_hpInnerFill.color = Color.white;
 
         // 0.2초 후에 원래 색상으로 되돌림
-        DOVirtual.DelayedCall(0.2f, () => IMG_hpInner.color = originalColor);
+        DOVirtual.DelayedCall(0.2f, () => IMG_hpInnerFill.color = originalColor);
 
         // 애니메이션으로 감소시키는 hpInner
         float newFillAmount = (float)_currHP / _maxHP;
-        DOTween.To(() => IMG_hpInner.fillAmount, x => IMG_hpInner.fillAmount = x, newFillAmount, 0.2f).SetEase(Ease.InCubic);
+        DOTween.To(() => SLD_hpInner.value, x => SLD_hpInner.value = x, newFillAmount, 0.2f).SetEase(Ease.InCubic);
     }
 
     public void UpdateHpBar()
     {
         // 바로 감소시키는 hpBar
-        IMG_hpBar.fillAmount    = (float)_currHP / _maxHP;
-        IMG_hpInner.fillAmount  = (float)_currHP / _maxHP;
+        SLD_hpBar.value    = (float)_currHP / _maxHP;
+        SLD_hpInner.value  = (float)_currHP / _maxHP;
     }
 }

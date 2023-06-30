564464ea78816ab83d76608301d628cfec1e7190
Yodle_94 <sj_artist@naver.com>
2023-06-30 14:33:13 +0900

    [서버 / #14 ] 약네트워크에서 event 추가될 때, 중복 등록될 가능성 있음.

--- HifeSurvival/RealtimeServer/ServerCore/Listener.cs
+++ HifeSurvival/RealtimeServer/ServerCore/Listener.cs
@@ -14,7 +14,7 @@ namespace ServerCore
 		public void Init(IPEndPoint endPoint, Func<Session> sessionFactory, int register = 10, int backlog = 100)
 		{
 			_listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-			_sessionFactory += sessionFactory;
+			_sessionFactory = sessionFactory;
 
             // 문지기 교육
             _listenSocket.Bind(endPoint);

67461a313435d304b7d386997a131abc9ee42c10
KKangTaee <honeypipeline@gmail.com>
2023-07-01 07:05:52 +0900

    [클라/수정/UI] 골드 획득 시 UI 추가 작업

--- HifeSurvival/Assets/Scripts/Charactes/ActionDisplayUI.cs
+++ HifeSurvival/Assets/Scripts/Charactes/ActionDisplayUI.cs
@@ -14,20 +14,22 @@ public class ActionDisplayUI : MonoBehaviour
     {
         ATTACK,
         TAKE_DAMAGE,
+        GET_GOLD,
     }
 
 
     [SerializeField] private TMP_Text TMP_attack;  // Text Component
     [SerializeField] private TMP_Text TMP_takeDamage;
+    [SerializeField] private TMP_Text TMP_getGold;
 
     public Vector3 startPos;    // Starting Position
     public Vector3 endPos;      // Ending Position
     public float height;        // Height of the bounce
     public float duration;      // Duration of the animation
 
-    public void PlayAttack(int inAttackVal, Action doneCallback)
+    public void PlayAttack(int attackVal, Action doneCallback)
     {
-        TMP_attack.text = inAttackVal.ToString();
+        TMP_attack.text = attackVal.ToString();
 
         // Create the waypoints for the animation
         Vector3[] waypoints = new Vector3[]
@@ -59,9 +61,9 @@ public class ActionDisplayUI : MonoBehaviour
     }
 
 
-    public void PlayDamaged(int inDamagedVal, Action doneCallback)
+    public void PlayDamaged(int damageVal, Action doneCallback)
     {
-        TMP_takeDamage.text = inDamagedVal.ToString(); // Show the damaged value
+        TMP_takeDamage.text = damageVal.ToString(); // Show the damaged value
         TMP_takeDamage.color = new Color(TMP_takeDamage.color.r, TMP_takeDamage.color.g, TMP_takeDamage.color.b, 0); // Set the alpha to 0
 
         Sequence s = DOTween.Sequence();
@@ -77,27 +79,58 @@ public class ActionDisplayUI : MonoBehaviour
     }
 
 
+    public void PlayGetGold(int gold, Action doneCallback)
+    {
+        float duration      = 1f; 
+        float moveY         = 2f; 
+        float fadeDuration  = 0.5f;
+
+        TMP_getGold.text = $"+{gold}";
+        var canvasGroup = TMP_getGold.GetComponent<CanvasGroup>();
+
+        // 처음 상태를 초기화
+        TMP_getGold.transform.localPosition = Vector3.zero;
+        canvasGroup.alpha = 1;
+
+        // 애니메이션을 순차적으로 진행
+        Sequence seq = DOTween.Sequence();
+        
+        // 이동 애니메이션 추가
+        seq.Append(TMP_getGold.transform.DOLocalMoveY(moveY, duration));
+
+        // 페이드 아웃 애니메이션 추가
+        seq.Insert(duration - fadeDuration, canvasGroup.DOFade(0, fadeDuration));
+
+        // 모든 애니메이션이 끝나면 콜백 함수 호출
+        seq.OnComplete(() => doneCallback?.Invoke());
+    }
+
+
     //---------------
     // static
     //---------------
 
-    public static void Show(ESpawnType inType, in int inVal, in Vector3 inPos)
+    public static void Show(ESpawnType type, in int val, in Vector3 pos)
     {
         var objectPool = ControllerManager.Instance.GetController<ObjectPoolController>();
         var inst = objectPool.SpawnFromPool<ActionDisplayUI>();
 
-        switch (inType)
+        switch (type)
         {
             case ESpawnType.ATTACK:
-                inst.transform.position = inPos;
-                inst.PlayAttack(inVal, () => objectPool.StoreToPool(inst));
+                inst.transform.position = pos;
+                inst.PlayAttack(val, () => objectPool.StoreToPool(inst));
                 break;
 
             case ESpawnType.TAKE_DAMAGE:
                 int offsetX = UnityEngine.Random.Range(-5, 5);
                 // int offsetY = UnityEngine.Random.Range(-3, 3);
-                inst.transform.position = inPos + new Vector3(offsetX * 0.1f, 0, 0);
-                inst.PlayDamaged(inVal, () => objectPool.StoreToPool(inst));
+                inst.transform.position = pos + new Vector3(offsetX * 0.1f, 0, 0);
+                inst.PlayDamaged(val, () => objectPool.StoreToPool(inst));
+                break;
+
+            case ESpawnType.GET_GOLD:
+                
                 break;
         }
     }

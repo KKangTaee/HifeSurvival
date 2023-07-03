6ae49380903452c003fe828608630f89c58482e6
KKangTaee <honeypipeline@gmail.com>
2023-07-03 22:27:30 +0900

    [클라/수정/이동] 오브젝트 이동 관련 수정
    * Stop처리 코드 부분 수정

--- HifeSurvival/Assets/Scripts/EntityObject/EntityObject.cs
+++ HifeSurvival/Assets/Scripts/EntityObject/EntityObject.cs
@@ -79,11 +79,13 @@ public abstract class EntityObject : MonoBehaviour
 
     public void MoveLerpExpect(in Vector3 inCurrPos, in Vector3 inDestPos, float inSpeed, long inTimeStamp, Action doneCallback = null)
     {
+        _moveMachine.StopMoveLerpExpect();
         _moveMachine.StartMoveLerpExpect(inCurrPos, inDestPos, inSpeed, inTimeStamp, doneCallback);
     }
 
     public void MoveLerpTarget(EntityObject inTarget, float inSpeed, Func<bool> inStopFunc, Action<Vector3> dirCallback, Action doneCallback)
     {
+        _moveMachine.StopMoveLerpTarget();
         _moveMachine.StartMoveLerpTarget(inTarget, inSpeed, inStopFunc, dirCallback, doneCallback);
     }
 
@@ -96,21 +98,11 @@ public abstract class EntityObject : MonoBehaviour
 
 
     [SerializeField] private SimplePoint _pointPrefab;
-    private List<SimplePoint> _pointList = new List<SimplePoint>();
 
     public void SetPoint(Vector3 pos, Color color)
     {
         var inst = Instantiate(_pointPrefab);
         inst.SetPoint(pos, color);
-        _pointList.Add(inst);
-    }
-
-    public void RemovePoint()
-    {
-        foreach (var point in _pointList)
-            Destroy(point);
-
-        _pointList.Clear();
     }
 }
 

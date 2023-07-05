45924b24e45fed2d40b43e1cccd62398b4330782
Yodle_94 <sj_artist@naver.com>
2023-07-05 15:47:33 +0900

    [ 서버 ] 테스트 클라이언트 구현 중

---
+++ HifeSurvival/TestClient/TestClient/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using ServerCore;
+
+namespace TestClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList.First(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7777);
+            // System.Console.WriteLine(ipAddr.Address);
+
+            PacketManager.Instance.BindHandler(new ClientPacketHandler());
+
+            Connector connector = new Connector();
+
+            connector.Connect(endPoint,
+                () => { return SessionManager.Instance.Generate(); },
+                1);
+        }
+    }
+}
